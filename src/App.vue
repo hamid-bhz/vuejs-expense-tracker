493d58a66ac6c0717bdd6896f772244a6354b5c4
Hamidreza Behzadi <hamidbehzadi71@gmail.com>
2024-03-07 19:37:07 +0330

    feat: add basic components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AddTransaction from './components/AddTransaction.vue'
+import Balance from './components/Balance.vue'
+import Header from './components/Header.vue'
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue'
+</script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <Header />
 
-  <main></main>
+  <main class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </main>
 </template>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,7 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li class="minus">Cash <span>-$400</span><button class="delete-btn">x</button></li>
+    <li class="plus">Paycheck <span>$200</span><button class="delete-btn">x</button></li>
+  </ul>
+</template>
