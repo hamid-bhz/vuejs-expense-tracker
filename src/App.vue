2fad28222f92e6ebf7b61ad45bc89ff6756bf101
Hamidreza Behzadi <hamidbehzadi71@gmail.com>
2024-03-07 19:13:21 +0330

    feat: init app

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main></main>
+</template>
